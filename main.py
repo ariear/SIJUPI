e4f6baa368ace9f3ea862d7884738653b90c8f3e
Arie <arieinjector@gmail.com>
2024-11-20 05:16:27 +0700

    feat(add): init menu & some changes

--- main.py
+++ main.py
@@ -1,14 +1,10 @@
 import os
-from menu import menu_autentikasi
+from menu import menu_autentikasi, menu_awal
 
 def main():
     os.system('cls')
     print(f"{'SELAMAT DATANG DI TOKO PUPUK DAN ALAT TANI':^90}\n")
 
-    menu_auth = menu_autentikasi()
-    if len(menu_auth) == 2:
-        print("MENU TOKO CUY")
-    elif menu_auth == 'keluar':
-        return 'keluar'
+    menu_awal()
 
 main()
--- menu.py
+++ menu.py
@@ -1,7 +1,34 @@
 import os
 from auth import login, register
 
+def menu_awal():
+    while True:
+        print("Kamu berada di menu tamu\n1. Daftar Pupuk dan Alat Pertanian\n2. Menu Autentikasi\n3. Keluar")
+        pilih_menu = input("Pilih berdasarkan nomor : ")
+        
+        match pilih_menu:
+            case '1':
+                os.system('cls')
+                print('gas daftar produk')
+                return 'daftar produk'
+            case '2':
+                menu_auth = menu_autentikasi()
+
+                if len(menu_auth) == 2:
+                    print("MENU TOKO CUY")
+                    return 'gas menu toko'
+                elif menu_auth == 'keluar':
+                    return 'keluar'
+            case '3':
+                print('\nTerimakasih sudah berkunjung di toko kami 🙏')
+                return 'keluar'
+            case _:
+                os.system('cls')
+                print('\nInput harus ada di menu dan berupa angka!\n')
+                continue
+
 def menu_autentikasi():
+    os.system('cls')
     while True:
         print("Kamu berada di menu autentikasi")
         print("1. Masuk ke akun\n2. Buat akun\n3. Keluar\n")

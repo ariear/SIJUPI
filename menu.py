9f982e6286e841582105214a0f0bc77f0e70888a
Arie <arieinjector@gmail.com>
2024-11-21 08:45:35 +0700

    feat(add): some changes

--- menu.py
+++ menu.py
@@ -60,11 +60,68 @@ def menu_utama(data_account):
 
     account = data_account
 
-    print(f"\n{'MENU TOKO SIJUPI':^50}\n\nSelamat datang {account[0]}✨\n\nBerikut adalah daftar menu untuk {role_parse(account[1])}")
+    while True:
+        print(f"\n{'MENU TOKO SIJUPI':^50}\n\nSelamat datang {account[0]}✨\n\nBerikut adalah daftar menu untuk {role_parse(account[1])}")
 
-    if account[1] == 0:
-        print("1. Kelola Pupuk dan Alat Tani\n2. Konfirmasi Pembelian\n3. Kelola Pembelian\n4. Kelola Pengeluaran Toko\n5. Laporan Penjualan dan Pengeluaran\n6. Lihat Notifikasi\n7. Kelola akun\n8. Perbarui Nomor Rekening Toko")
-    elif account[1] == 1:
-        print("1. Kelola Pupuk dan Alat Tani\n2. Konfirmasi Pembelian\n3. Kelola Pembelian\n4. Kelola Pengeluaran Toko\n5. Laporan Penjualan dan Pengeluaran\n6. Lihat Notifikasi")
-    elif account[1] == 2:
-        print("1. Beli Pupuk dan Alat Pertanian\n2. Daftar Pembelian\n3. Info Akun\n4. Wishlist Barang\n5. Lihat Notifikasi")
+        if account[1] == 0:
+            print("1. Kelola Pupuk dan Alat Tani\n2. Konfirmasi Pembelian\n3. Kelola Pembelian\n4. Kelola Pengeluaran Toko\n5. Laporan Penjualan dan Pengeluaran\n6. Lihat Notifikasi\n7. Kelola akun\n8. Perbarui Nomor Rekening Toko")
+        
+            pilih_menu = input("Pilih berdasarkan nomor : ")
+            if pilih_menu == '1':
+                print('kelola pupuk ')
+            elif pilih_menu == '2':
+                print('konfir pembeli')
+            elif pilih_menu == '3':
+                print('kelola pembelian')
+            elif pilih_menu == '4':
+                print('kelola pengeluaran toko')
+            elif pilih_menu == '5':
+                print('laporan penjualan dan pengeluaran')
+            elif pilih_menu == '6':
+                print('lihat notif')
+            elif pilih_menu == '7':
+                print('kelola akun')
+            elif pilih_menu == '8':
+                print('update no rek')
+            else:
+                os.system('cls')
+                print('Input harus ada di menu dan berupa angka!')
+                continue
+        elif account[1] == 1:
+            print("1. Kelola Pupuk dan Alat Tani\n2. Konfirmasi Pembelian\n3. Kelola Pembelian\n4. Kelola Pengeluaran Toko\n5. Laporan Penjualan dan Pengeluaran\n6. Lihat Notifikasi")
+
+            pilih_menu = input("Pilih berdasarkan nomor : ")
+            if pilih_menu == '1':
+                print('kelola pupuk ')
+            elif pilih_menu == '2':
+                print('konfir pembeli')
+            elif pilih_menu == '3':
+                print('kelola pembelian')
+            elif pilih_menu == '4':
+                print('kelola pengeluaran toko')
+            elif pilih_menu == '5':
+                print('laporan penjualan dan pengeluaran')
+            elif pilih_menu == '6':
+                print('lihat notif')
+            else:
+                os.system('cls')
+                print('Input harus ada di menu dan berupa angka!')
+                continue
+        elif account[1] == 2:
+            print("1. Beli Pupuk dan Alat Pertanian\n2. Daftar Pembelian\n3. Info Akun\n4. Wishlist Barang\n5. Lihat Notifikasi")
+
+            pilih_menu = input("Pilih berdasarkan nomor : ")
+            if pilih_menu == '1':
+                print('beli pupuk ')
+            elif pilih_menu == '2':
+                print('daftar beli')
+            elif pilih_menu == '3':
+                print('info akun')
+            elif pilih_menu == '4':
+                print('wislis')
+            elif pilih_menu == '5':
+                print('notif')
+            else:
+                os.system('cls')
+                print('Input harus ada di menu dan berupa angka!')
+                continue
